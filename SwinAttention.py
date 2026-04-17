b6ad96e068e9293b50394840a3cc3ae82bae63c2
Yakov Danishevsky <yakovdan@gmail.com>
2026-04-17 21:09:03 +0300

    kickoff

---
+++ SwinAttention.py
@@ -0,0 +1,235 @@
+from __future__ import annotations
+from collections.abc import Sequence
+import torch
+from torch import nn
+import math
+from torch.nn.attention import SDPBackend, sdpa_kernel
+import torch.nn.functional as F
+from monai.networks.nets.swin_unetr import WindowAttention as OrigWindowAttention
+import numpy as np
+import random
+
+def _no_grad_trunc_normal_(tensor, mean, std, a, b):
+    """Tensor initialization with truncated normal distribution.
+    Based on:
+    https://people.sc.fsu.edu/~jburkardt/presentations/truncated_normal.pdf
+    https://github.com/rwightman/pytorch-image-models
+
+    Args:
+       tensor: an n-dimensional `torch.Tensor`.
+       mean: the mean of the normal distribution.
+       std: the standard deviation of the normal distribution.
+       a: the minimum cutoff value.
+       b: the maximum cutoff value.
+    """
+
+    def norm_cdf(x):
+        return (1.0 + math.erf(x / math.sqrt(2.0))) / 2.0
+
+    with torch.no_grad():
+        l = norm_cdf((a - mean) / std)
+        u = norm_cdf((b - mean) / std)
+        tensor.uniform_(2 * l - 1, 2 * u - 1)
+        tensor.erfinv_()
+        tensor.mul_(std * math.sqrt(2.0))
+        tensor.add_(mean)
+        tensor.clamp_(min=a, max=b)
+        return tensor
+
+
+def trunc_normal_(tensor, mean=0.0, std=1.0, a=-2.0, b=2.0):
+    """Tensor initialization with truncated normal distribution.
+    Based on:
+    https://github.com/rwightman/pytorch-image-models
+
+    Args:
+       tensor: an n-dimensional `torch.Tensor`
+       mean: the mean of the normal distribution
+       std: the standard deviation of the normal distribution
+       a: the minimum cutoff value
+       b: the maximum cutoff value
+    """
+
+    if std <= 0:
+        raise ValueError("the standard deviation should be greater than zero.")
+
+    if a >= b:
+        raise ValueError("minimum cutoff value (a) should be smaller than maximum cutoff value (b).")
+
+    return _no_grad_trunc_normal_(tensor, mean, std, a, b)
+
+
+class WindowAttention(nn.Module):
+    """
+    Window based multi-head self attention module with relative position bias based on: "Liu et al.,
+    Swin Transformer: Hierarchical Vision Transformer using Shifted Windows
+    <https://arxiv.org/abs/2103.14030>"
+    https://github.com/microsoft/Swin-Transformer
+    """
+
+    def __init__(
+        self,
+        dim: int,
+        num_heads: int,
+        window_size: Sequence[int],
+        qkv_bias: bool = False,
+        attn_drop: float = 0.0,
+        proj_drop: float = 0.0,
+    ) -> None:
+        """
+        Args:
+            dim: number of feature channels.
+            num_heads: number of attention heads.
+            window_size: local window size.
+            qkv_bias: add a learnable bias to query, key, value.
+            attn_drop: attention dropout rate.
+            proj_drop: dropout rate of output.
+        """
+
+        super().__init__()
+        self.dim = dim
+        self.window_size = window_size
+        self.num_heads = num_heads
+        head_dim = dim // num_heads
+        self.scale = head_dim**-0.5
+        mesh_args = torch.meshgrid.__kwdefaults__
+
+        if len(self.window_size) == 3:
+            self.relative_position_bias_table = nn.Parameter(
+                torch.zeros(
+                    (2 * self.window_size[0] - 1) * (2 * self.window_size[1] - 1) * (2 * self.window_size[2] - 1),
+                    num_heads,
+                )
+            )
+            coords_d = torch.arange(self.window_size[0])
+            coords_h = torch.arange(self.window_size[1])
+            coords_w = torch.arange(self.window_size[2])
+            if mesh_args is not None:
+                coords = torch.stack(torch.meshgrid(coords_d, coords_h, coords_w, indexing="ij"))
+            else:
+                coords = torch.stack(torch.meshgrid(coords_d, coords_h, coords_w))
+            coords_flatten = torch.flatten(coords, 1)
+            relative_coords = coords_flatten[:, :, None] - coords_flatten[:, None, :]
+            relative_coords = relative_coords.permute(1, 2, 0).contiguous()
+            relative_coords[:, :, 0] += self.window_size[0] - 1
+            relative_coords[:, :, 1] += self.window_size[1] - 1
+            relative_coords[:, :, 2] += self.window_size[2] - 1
+            relative_coords[:, :, 0] *= (2 * self.window_size[1] - 1) * (2 * self.window_size[2] - 1)
+            relative_coords[:, :, 1] *= 2 * self.window_size[2] - 1
+
+        elif len(self.window_size) == 2:
+            self.relative_position_bias_table = nn.Parameter(
+                torch.zeros((2 * window_size[0] - 1) * (2 * window_size[1] - 1), num_heads)
+            )
+            coords_h = torch.arange(self.window_size[0])
+            coords_w = torch.arange(self.window_size[1])
+            if mesh_args is not None:
+                coords = torch.stack(torch.meshgrid(coords_h, coords_w, indexing="ij"))
+            else:
+                coords = torch.stack(torch.meshgrid(coords_h, coords_w))
+            coords_flatten = torch.flatten(coords, 1)
+            relative_coords = coords_flatten[:, :, None] - coords_flatten[:, None, :]
+            relative_coords = relative_coords.permute(1, 2, 0).contiguous()
+            relative_coords[:, :, 0] += self.window_size[0] - 1
+            relative_coords[:, :, 1] += self.window_size[1] - 1
+            relative_coords[:, :, 0] *= 2 * self.window_size[1] - 1
+        else:
+            raise ValueError(f"length of window_size must be either 2 or 3, got {len(window_size)}")
+        relative_position_index = relative_coords.sum(-1)
+        self.register_buffer("relative_position_index", relative_position_index)
+        self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
+        self.attn_drop = nn.Dropout(attn_drop)
+        self.proj = nn.Linear(dim, dim)
+        self.proj_drop = nn.Dropout(proj_drop)
+        trunc_normal_(self.relative_position_bias_table, std=0.02)
+        self.softmax = nn.Softmax(dim=-1)
+
+    def forward(self, x, mask):
+        b, n, c = x.shape
+        qkv = self.qkv(x).reshape(b, n, 3, self.num_heads, c // self.num_heads).permute(2, 0, 3, 1, 4)
+        d = c // self.num_heads
+        q, k, v = qkv[0], qkv[1], qkv[2]
+
+        relative_position_bias = self.relative_position_bias_table[
+            self.relative_position_index.clone()[:n, :n].reshape(-1)  # type: ignore[operator]
+        ].reshape(n, n, -1)
+        rpb = relative_position_bias.permute(2, 0, 1).unsqueeze(0).contiguous()
+        if mask is not None:
+
+            nw = mask.shape[0]
+            g = b // nw
+
+            if self.training:
+                attn = (q * self.scale) @ k.transpose(-2, -1)
+                attn = attn + rpb
+                attn = attn.view(b // nw, nw, self.num_heads, n, n) + mask.unsqueeze(0).unsqueeze(2)
+                attn = attn.view(-1, self.num_heads, n, n)
+                attn = self.softmax(attn)
+                attn = self.attn_drop(attn).to(v.dtype)
+                x = (attn @ v).transpose(1, 2).reshape(b, n, c)
+            else:
+                # q, k, v assumed to start as [b, num_heads, n, d]
+                q1 = q.view(g, nw, self.num_heads, n, d)
+                k1 = k.view(g, nw, self.num_heads, n, d)
+                v1 = v.view(g, nw, self.num_heads, n, d)
+
+                with sdpa_kernel([SDPBackend.FLASH_ATTENTION,SDPBackend.CUDNN_ATTENTION,SDPBackend.MATH], set_priority=True):
+                    attn = [F.scaled_dot_product_attention(
+                        q1[i], k1[i], v1[i],
+                        attn_mask=rpb + mask.unsqueeze(1),
+                        dropout_p=0.0,
+                        scale=float(self.scale),
+                    ) for i in range(g)]
+                x = torch.cat(attn).transpose(-2, -3).reshape(b, n, c)
+
+        else:
+            if self.training:
+                attn = q @ k.transpose(-2, -1)
+                attn = attn + rpb
+                attn = self.softmax(attn)
+                attn = self.attn_drop(attn).to(v.dtype)
+                x = (attn @ v).transpose(1, 2).reshape(b, n, c)
+            else:
+                with sdpa_kernel([SDPBackend.CUDNN_ATTENTION], set_priority=True):
+                    attn = F.scaled_dot_product_attention(
+                        q, k, v,
+                        attn_mask=rpb,
+                        dropout_p=0.0,
+                        scale=float(self.scale))
+
+                x = attn.transpose(-2, -3).reshape(b, n, c)
+
+        x = self.proj(x)
+        x = self.proj_drop(x)
+        return x
+
+
+if __name__ == "__main__":
+
+    # ----------------------------
+    # Reproducibility
+    # ----------------------------
+    SEED = 1234
+    random.seed(SEED)
+    np.random.seed(SEED)
+    torch.manual_seed(SEED)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(SEED)
+
+    x = torch.load('input_tensor.pt').to("cuda").to(torch.bfloat16)
+    mask = torch.load('mask_tensor.pt').to("cuda").to(torch.bfloat16)
+    win0 = WindowAttention(48, 3, (7, 7, 7), True, 0.25, 0.25).to("cuda").to(torch.bfloat16)
+    win1 = OrigWindowAttention(48, 3, (7, 7, 7), True, 0.25, 0.25).to("cuda").to(torch.bfloat16)
+    win0.eval()
+    win1.eval()
+    with torch.inference_mode():
+        out0 = win0(x, mask)
+        SEED = 1234
+        random.seed(SEED)
+        np.random.seed(SEED)
+        torch.manual_seed(SEED)
+        if torch.cuda.is_available():
+            torch.cuda.manual_seed_all(SEED)
+
+        out1 = win1(x, mask)
+    print(torch.allclose(out0, out1))
