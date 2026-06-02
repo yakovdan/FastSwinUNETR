fixed to precision of 5e-6

triton_fa_updated_test.py
@@ -566,8 +566,8 @@ def test_op(
     HEAD_DIM: int,
     causal: bool,
     dtype: torch.dtype = torch.float16,
-    atol: float = 1e-6,
-    rtol: float = 1e-6,
+    atol: float = 5e-6,
+    rtol: float = 0,
 ) -> None:
     torch.manual_seed(1234)
     torch.cuda.manual_seed_all(1234)
@@ -669,7 +669,7 @@ if __name__ == "__main__":
 
     # Your actual dimensions.
     test_op(BATCH_SIZE=1176, NUM_HEADS=3, SEQ_LEN=343, HEAD_DIM=16, causal=False, dtype=torch.float32)
-
+    test_op(BATCH_SIZE=1176, NUM_HEADS=3, SEQ_LEN=343, HEAD_DIM=16, causal=True, dtype=torch.float32)
     # Optional forward benchmark.
     benchmark_forward(BATCH_SIZE=1176, NUM_HEADS=3, SEQ_LEN=343, HEAD_DIM=16, causal=False, dtype=torch.float32)
 